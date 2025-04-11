52708d69c745ec914efe3729d7b9ec91c228b5ca
Phil Birch <p.m.birch@sussex.ac.uk>
2025-04-11 16:38:09 +0100

    added compression

---
+++ mass.bash
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python mass_face_eater.py --input_directory /root/Documents/ZED/ZED/Real_data/day1/1 --output_directory /root/Documents/processed/real_proc1/1 --compress_point_cloud ---no_display
